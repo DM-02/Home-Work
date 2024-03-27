6f31146bb23c2569e4506aa142aaf7cf8fcdce6e
Dmitry <142209152+DM-02@users.noreply.github.com>
2024-03-27 10:34:10 +0500

    Update main.cpp

--- Home Work/main.cpp
+++ Home Work/main.cpp
@@ -6,9 +6,42 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <random>
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::fstream inputFile("input.txt");
+    std::ofstream outputFile("output.txt");
+    
+    std::string line;
+    std::queue<std::string> namesQueue;
+    
+    //!Счтитываем имена из файла и сохраняем их в очередь
+    while(std::getline(inputFile, line)) {
+        namesQueue.push(line);
+    }
+    //!Перемешиваем имена в очередь
+    std::vector<std::string> shuffledNames;
+    while (!namesQueue.empty()) {
+        shuffledNames.push_back(namesQueue.front());
+        namesQueue.pop();
+    }
+    std::shuffle(shuffledNames.begin(),shuffledNames.end(),std::mt19937(std::random_device()()));
+    
+    //!Выводим перемешанные имена на экран и записываем в файл
+    for(const std::string& name : shuffledNames){
+        std::cout << name << std::endl;
+        outputFile << name << std::endl;
+    }
+    
+    inputFile.close();
+    outputFile.close();
+    
     return 0;
 }
+
